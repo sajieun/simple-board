0d035a08fb2b9c524962b010f76bd12fdaf2f4e8
sajieun <sajieun000721@naver.com>
2024-05-27 01:28:13 +0900

    reply db create

---
+++ src/main/java/com/example/simple_board/reply/db/ReplyEntity.java
@@ -0,0 +1,42 @@
+package com.example.simple_board.reply.db;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity(name = "reply")
+public class ReplyEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String userName;
+
+    private String password;
+
+    private String email;
+
+    private String status;
+
+    private String title;
+
+    private String content;
+
+    private LocalDateTime postedAt;
+
+
+
+
+}
