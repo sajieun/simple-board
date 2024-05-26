dd02bdbde80302b0b28dd3b3fd1dbf42c0520947
sajieun <sajieun000721@naver.com>
2024-05-27 02:17:01 +0900

    + @RequestBody

--- src/main/java/com/example/simple_board/post/controller/PostController.java
+++ src/main/java/com/example/simple_board/post/controller/PostController.java
@@ -7,6 +7,7 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.Value;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,7 +21,7 @@ public class PostController {
     @PostMapping("")
     public PostEntity create(
             @Valid
-            PostRequest postRequest
+            @RequestBody PostRequest postRequest
     ){
         return postService.create(postRequest);
     }
