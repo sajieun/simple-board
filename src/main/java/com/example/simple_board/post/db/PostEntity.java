005f46eaa7b3c7b56cdc72e3f25ba641a2ca3d2e
sajieun <sajieun000721@naver.com>
2024-05-27 01:24:15 +0900

    post db create

---
+++ src/main/java/com/example/simple_board/post/db/PostEntity.java
@@ -0,0 +1,41 @@
+package com.example.simple_board.post.db;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.sql.Date;
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity(name = "post")
+public class PostEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long boardId;
+
+    private String userName;
+
+    private String password;
+
+    private String email;
+
+    private String status;
+
+    private String title;
+
+    private String content;
+
+    private LocalDateTime postedAt;
+}
