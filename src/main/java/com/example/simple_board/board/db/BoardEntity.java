db8b7f7346634c0b78a8da2d276092e063a88a92
sajieun <sajieun000721@naver.com>
2024-05-27 01:13:42 +0900

    board db create

---
+++ src/main/java/com/example/simple_board/board/db/BoardEntity.java
@@ -0,0 +1,29 @@
+package com.example.simple_board.board.db;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity(name = "board")
+public class BoardEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+
+    private String boardName;
+
+    private String status;
+
+
+}
