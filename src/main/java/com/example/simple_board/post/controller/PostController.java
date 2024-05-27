e01d0d4bd75bd27e5b1d6c326c82c213ccae18a2
sajieun <sajieun000721@naver.com>
2024-05-27 12:41:42 +0900

    controller view, all, delete coding

--- src/main/java/com/example/simple_board/post/controller/PostController.java
+++ src/main/java/com/example/simple_board/post/controller/PostController.java
@@ -1,15 +1,16 @@
 package com.example.simple_board.post.controller;
 
 import com.example.simple_board.post.db.PostEntity;
+import com.example.simple_board.post.db.PostRepository;
 import com.example.simple_board.post.model.PostRequest;
+import com.example.simple_board.post.model.PostViewRequest;
 import com.example.simple_board.post.service.PostService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.Value;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/post")
@@ -17,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class PostController {
 
     private final PostService postService;
+    private final PostRepository postRepository;
 
     @PostMapping("")
     public PostEntity create(
@@ -24,5 +26,30 @@ public class PostController {
             @RequestBody PostRequest postRequest
     ){
         return postService.create(postRequest);
+    }
+
+    // 원래는 view는 getMapping인데 자신의 비밀번호를 입력해야지만 볼 수 있어서 postMapping으로 해준다.
+    @PostMapping("/view")
+    public PostEntity view(
+
+            @Valid
+            @RequestBody PostViewRequest postViewRequest
+    ){
+        return postService.view(postViewRequest);
+
+    }
+
+    @GetMapping("/all")
+    public List<PostEntity> list(
+    ){
+        return postService.all();
+    }
+
+    @PostMapping("/delete")
+    public void delete(
+            @Valid
+            @RequestBody PostViewRequest postViewRequest
+    ){
+        postService.delete(postViewRequest);
     }
 }
--- src/main/java/com/example/simple_board/post/service/PostService.java
+++ src/main/java/com/example/simple_board/post/service/PostService.java
@@ -3,10 +3,12 @@ package com.example.simple_board.post.service;
 import com.example.simple_board.post.db.PostEntity;
 import com.example.simple_board.post.db.PostRepository;
 import com.example.simple_board.post.model.PostRequest;
+import com.example.simple_board.post.model.PostViewRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -31,5 +33,45 @@ public class PostService {
 
         return postRepository.save(entity);
 
+    }
+   /*
+            1. 게시글이 있는가?
+            2. 비밀번호가 맞는가?*/
+    public PostEntity view(PostViewRequest postViewRequest) {
+         return postRepository.findById(postViewRequest.getPostId())
+                .map(it ->{
+                    // entity 존재
+                    if (!it.getPassword().equals(postViewRequest.getPassword())){
+                        var format = "패스워드가 맞지 않습니다 %s vs %s";
+                        throw new RuntimeException(String.format(format,it.getPostedAt(),postViewRequest.getPassword()));
+                    }
+                    return it;
+                }).orElseThrow(
+                        () -> {
+                            return new RuntimeException("해당 게시글이 존재하지 않습니다. " + postViewRequest.getPostId());
+                        }
+                );
+    }
+
+    public List<PostEntity> all() {
+        return postRepository.findAll();
+    }
+
+    public void delete(PostViewRequest postViewRequest) {
+        postRepository.findById(postViewRequest.getPostId())
+                .map(it ->{
+                    // entity 존재
+                    if (!it.getPassword().equals(postViewRequest.getPassword())){
+                        var format = "패스워드가 맞지 않습니다 %s vs %s";
+                        throw new RuntimeException(String.format(format,it.getPostedAt(),postViewRequest.getPassword()));
+                    }
+                    it.setStatus("UNREGISTERED");
+                    postRepository.save(it);
+                    return it;
+                }).orElseThrow(
+                        () -> {
+                            return new RuntimeException("해당 게시글이 존재하지 않습니다. " + postViewRequest.getPostId());
+                        }
+                );
     }
 }
