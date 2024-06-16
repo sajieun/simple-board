42a22562b8d36521a73ce9eaa1473c4d22536fbe
sajieun <sajieun000721@naver.com>
2024-06-16 19:46:04 +0900

    CURDAbstractService create

--- src/main/java/com/example/simple_board/crud/CRUDAbstactService.java
+++ src/main/java/com/example/simple_board/crud/CRUDAbstactService.java
@@ -1,4 +1,86 @@
 package com.example.simple_board.crud;
 
-public class CRUDAbstactService {
+import com.example.simple_board.common.Api;
+import com.example.simple_board.common.Pagination;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.awt.print.Pageable;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+// dto -> entity -> dto
+public class CRUDAbstactService<DTO, ENTITY> implements CRUDInterface<DTO> {
+
+    @Autowired(required = false)
+    private JpaRepository<ENTITY,Long> jpaRepository;
+
+    @Autowired(required = false)
+    private Converter<DTO, ENTITY> converter;
+
+    @Override
+    public DTO create(DTO dto) {
+
+        // dto -> entity
+        var entity = converter.toEntity(dto);
+        // entity -> save
+        jpaRepository.save(entity);
+
+        // save -> dto
+        var returnDto = converter.toDto(entity);
+
+        return returnDto;
+    }
+
+    @Override
+    public Optional<DTO> read(Long id) {
+
+        var optionalENTITY = jpaRepository.findById(id);
+
+        var dto = optionalENTITY.map(
+                it -> converter.toDto(it)
+        ).orElseGet(() -> null);
+
+        return Optional.ofNullable(dto);
+    }
+
+    @Override
+    public DTO updadte(DTO dto) {
+        var entity = converter.toEntity(dto);
+        jpaRepository.save(entity);
+        var returnDto = converter.toDto(entity);
+        return returnDto;
+    }
+
+    @Override
+    public void delete(Long id) {
+        jpaRepository.deleteById(id);
+
+    }
+
+    @Override
+    public Api<List<DTO>> list(Pageable pageable) {
+        var list = jpaRepository.findAll((org.springframework.data.domain.Pageable) pageable);
+
+        var pagination = Pagination.builder()
+                .page(list.getNumber())
+                .size(list.getSize())
+                .currentElements(list.getNumberOfElements())
+                .totalElements(list.getTotalElements())
+                .totalPage(list.getTotalPages())
+                .build()
+                ;
+
+        var dtoList = list.stream()
+                .map(it -> converter.toDto(it)).collect(Collectors.toList());
+
+        var response = Api.<List<DTO>>builder()
+                .body(dtoList)
+                .pagination(pagination)
+                .build()
+                ;
+        return response;
+    }
 }
